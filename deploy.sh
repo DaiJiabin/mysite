bd826f4a82acf59ed6ad8d6fa12d48d4a70cb0a8
Kechou.Dai <kechou.dai@gmail.com>
2021-04-26 17:17:58 +0200

    Processing 2021年04月26日 17:17:58

--- deploy.sh
+++ deploy.sh
@@ -20,3 +20,21 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+cd ..
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Processing $(date)"
+
+if [ -n "$*"  ]; then
+        msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push -u origin master
